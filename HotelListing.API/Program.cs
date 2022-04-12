2b80f7958e28d8610cb10dc88e106b67f2129511
Rodrigo Salvate Brasil <rodrigosalvate@hotmail.com>
2022-04-12 10:22:51 -0300

    Adicionar arquivos de projeto.

---
+++ HotelListing.API/Program.cs
@@ -0,0 +1,44 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Configura o CORS para aceitar qualquer request de qualquer fonte. 
+//CORS permite ajustar políticas para restringir acessos de fontes fora do servidor da API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyHeader()
+            .AllowAnyOrigin()
+            .AllowAnyMethod());
+});
+
+// https://datalust.co/seq
+// ctx é um avariavel para a instancia do builder
+//lc é para a configuração do log "log configuration"
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
