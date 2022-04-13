773b76de96697cd586f08d99fccbda981b737667
Rodrigo Salvate Brasil <rodrigosalvate@hotmail.com>
2022-04-12 21:28:27 -0300

    Adding CountriesControllers. Adding POST and GET endpoint

---
+++ HotelListing.API/Controllers/ContriesController.cs
@@ -0,0 +1,44 @@
+﻿using HotelListing.API.Data;
+using HotelListing.API.Data.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContriesController : ControllerBase
+    {
+        private readonly HotelListingDbContext _context;
+
+        public ContriesController(HotelListingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+
+        //POST: api/Countries
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCountry), new { id = country.Id }, country);
+        }
+
+    }
+}
